0be5c122b50ad543655a9e453577b8dfef093355
Mikaelyan Eduard <deliveosdars@gmail.com>
2024-03-12 04:19:51 +0500

    drop migration

--- internal/webappbot/migrate.go
+++ internal/webappbot/migrate.go
@@ -1,13 +1,7 @@
 package webappbot
 
 import (
-	"errors"
-	"log"
-	"os"
 	"time"
-
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/joho/godotenv"
 
 	// migrate tools
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
@@ -20,47 +14,47 @@ const (
 )
 
 func init() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-	databaseURL, ok := os.LookupEnv("PG_URL")
-	if !ok || len(databaseURL) == 0 {
-		log.Fatalf("migrate: environment variable not declared: PG_URL")
-	}
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// }
+	// databaseURL, ok := os.LookupEnv("PG_URL")
+	// if !ok || len(databaseURL) == 0 {
+	// 	log.Fatalf("migrate: environment variable not declared: PG_URL")
+	// }
 
-	databaseURL += "?sslmode=disable"
+	// databaseURL += "?sslmode=disable"
 
-	var (
-		attempts = _defaultAttempts
-		m        *migrate.Migrate
-	)
+	// var (
+	// 	attempts = _defaultAttempts
+	// 	m        *migrate.Migrate
+	// )
 
-	for attempts > 0 {
-		m, err = migrate.New("file://migrations", databaseURL)
-		if err == nil {
-			break
-		}
+	// for attempts > 0 {
+	// 	m, err = migrate.New("file://migrations", databaseURL)
+	// 	if err == nil {
+	// 		break
+	// 	}
 
-		log.Printf("Migrate: postgres is trying to connect, attempts left: %d", attempts)
-		time.Sleep(_defaultTimeout)
-		attempts--
-	}
+	// 	log.Printf("Migrate: postgres is trying to connect, attempts left: %d", attempts)
+	// 	time.Sleep(_defaultTimeout)
+	// 	attempts--
+	// }
 
-	if err != nil {
-		log.Fatalf("Migrate: postgres connect error: %s", err)
-	}
+	// if err != nil {
+	// 	log.Fatalf("Migrate: postgres connect error: %s", err)
+	// }
 
-	err = m.Up()
-	defer m.Close()
-	if err != nil && !errors.Is(err, migrate.ErrNoChange) {
-		log.Fatalf("Migrate: up error: %s", err)
-	}
+	// err = m.Up()
+	// defer m.Close()
+	// if err != nil && !errors.Is(err, migrate.ErrNoChange) {
+	// 	log.Fatalf("Migrate: up error: %s", err)
+	// }
 
-	if errors.Is(err, migrate.ErrNoChange) {
-		log.Printf("Migrate: no change")
-		return
-	}
+	// if errors.Is(err, migrate.ErrNoChange) {
+	// 	log.Printf("Migrate: no change")
+	// 	return
+	// }
 
-	log.Printf("Migrate: up success")
+	// log.Printf("Migrate: up success")
 }
